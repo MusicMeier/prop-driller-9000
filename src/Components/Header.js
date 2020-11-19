f2a8da8484a8b30163b2b8652b5bf18c3b7b6c4c
Music <musicm987@gmail.com>
2020-11-19 14:09:02 -0700

    Steps one and two

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
       <img
-        src = ""
+        src = "https://i.pinimg.com/originals/f8/7d/ec/f87dec9d6334af383a27f73e48ccf154.png"
         alt = "logo"
       />
     </header>
--- src/Containers/TransformersSection.js
+++ src/Containers/TransformersSection.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Transformer from '../Components/Transformer'
 
 class TransformersSection extends Component {
+
+  mappedTransformers = () => {
+    return this.props.transformers.map(transformer => {
+      return <Transformer transformer={transformer}/>
+    })
+  }
 
   render() {
     return (
       <section className = "transformers-section">
-        {/* Transformers here */}
+        {this.mappedTransformers()}
       </section>
     );
   }
